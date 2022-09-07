14924dbc1fc5ee80773de7f5028ed2171c0df433
Yauheni <overhawlin@gmail.com>
2022-09-07 17:06:23 +0200

    feat: implement keys remap

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Bi-Directional Adapter
-=========================
+# Bi-Directional Adapter
 
 [![npm version](https://img.shields.io/npm/v/bidirectional-adapter.svg?style=flat-square)](https://www.npmjs.com/package/bidirectional-adapter)
 [![npm downloads](https://img.shields.io/npm/dm/bidirectional-adapter.svg?style=flat-square)](https://www.npmjs.com/package/bidirectional-adapter)
@@ -17,8 +16,8 @@ With `bidirectional-adapter` you can define a simple entity which can be used to
 
 ```ts
 const adapter = createAdapter<string, number>(
-    (stringValue) => parseInt(stringValue, 10),
-    (numericValue) => String(numericValue)
+  (stringValue) => parseInt(stringValue, 10),
+  (numericValue) => String(numericValue)
 );
 ```
 
@@ -29,32 +28,82 @@ import axios from 'axios';
 import createAdapter from 'bidirectional-adapter';
 
 const accountAdapter = createAdapter(
-    (dbAccount) => ({
-        id: dbAccount.user_id,
-        name: `${dbAccount.user.firstName} ${dbAccount.user.lastName}`,
-        email: dbAccount.user.email
-    }),
-    (appAccount) => ({
-        user_id: appAcount.id,
-        user: {
-            firstName: appAccount.name.split(' ')[0],
-            lastName: appAccount.name.split(' ')[1]
-        },
-        email: appAccount.email
-    })
+  (dbAccount) => ({
+    id: dbAccount.user_id,
+    name: `${dbAccount.user.firstName} ${dbAccount.user.lastName}`,
+    email: dbAccount.user.email,
+  }),
+  (appAccount) => ({
+    user_id: appAcount.id,
+    user: {
+      firstName: appAccount.name.split(' ')[0],
+      lastName: appAccount.name.split(' ')[1],
+    },
+    email: appAccount.email,
+  })
 );
 
 const fetchAccount = (userID) => {
-    // state is in the shape of propertyTwo
-    // do reducer stuff here
-    return axios.get(`/account/${userID}`).then(res => accountAdapter.fromDB(res.data));
+  // state is in the shape of propertyTwo
+  // do reducer stuff here
+  return axios.get(`/account/${userID}`).then((res) => accountAdapter.fromDB(res.data));
 };
 
 const updateAccount = (account) => {
-    // state is in the shape of propertyTwo
-    // do reducer stuff here
-    return axios.post(`/account/${userID}`, accountAdapter.toDB(account));
+  // state is in the shape of propertyTwo
+  // do reducer stuff here
+  return axios.post(`/account/${userID}`, accountAdapter.toDB(account));
 };
+```
+
+## Smart Adapter Example
+
+```ts
+import { createSmartMultiAdapter } from 'bidirectional-adapter';
+
+interface DBModel {
+  x: number;
+  a: number;
+  b: string;
+  c1: boolean;
+}
+
+interface Model {
+  x: number;
+  ab: string;
+  c2: boolean;
+}
+
+type KeyMap = [['a' | 'b', 'ab'], ['c1', 'c2']];
+
+const adapter = createSmartMultiAdapter<DBModel, Model, [], [], KeyMap>(
+  () => ({} as any),
+  () => ({} as any)
+);
+
+adapter.fromDB({ a: 1, b: 'a', c1: false, x: 1 }); // Model
+adapter.fromDB({ a: 1, b: 'a', c1: false }); // Pick<Model, 'ab' | 'c2'>
+adapter.fromDB({ b: 'a', c1: false }); // Pick<Model, 'c2'>
+adapter.fromDB({ x: 1 }); // Pick<Model, 'x1'>
+adapter.fromDB({}); // EmptyObject
+
+adapter.mapFromDB([{ a: 1, b: 'a', c1: false, x: 1 }]); // Model[]
+adapter.mapFromDB([{ a: 1, b: 'a', c1: false }]); // Pick<Model, 'ab' | 'c2'>[]
+adapter.mapFromDB([{ b: 'a', c1: false }]); // Pick<Model, 'c2'>[]
+adapter.mapFromDB([{ x: 1 }]); // Pick<Model, 'x1'>[]
+adapter.mapFromDB([{}]); // EmptyObject[]
+
+adapter.toDB({ ab: '1', c2: false, x: 1 }); // DBModel
+adapter.toDB({ ab: '1', x: 1 }); // Pick<DBModel, 'a' | 'b' | 'x'>
+adapter.toDB({ c2: false }); // Pick<Model, 'c2'>
+adapter.toDB({ x: 1 }); // Pick<Model, 'x1'>
+adapter.toDB({}); // EmptyObject
+
+adapter.mapToDB([{ ab: '1', c2: false, x: 1 }]); // DBModel
+adapter.mapToDB([{ ab: '1', x: 1 }]); // Pick<DBModel, 'a' | 'b' | 'x'>
+adapter.mapToDB([{ c2: false }]); // Pick<Model, 'c2'>
+adapter.mapToDB([{ x: 1 }]); // Pick<Model, 'x1'>
+adapter.mapToDB([{}]); // EmptyObject
 ```
 
 ## Installation
--- src/adapter.ts
+++ src/adapter.ts
@@ -1,19 +1,61 @@
+type EmptyObject = Record<string, never>;
+
+type UnionToIntersection<Union> = (Union extends any ? (k: Union) => void : never) extends (k: infer Intersection) => void ? Intersection : never;
+
+type RemapFromDBUnion<
+  DBModel,
+  Model,
+  PartialDBModel extends Partial<DBModel>,
+  KeyRemap extends [keyof PartialDBModel, keyof Model]
+> = KeyRemap extends [infer ToDBKey extends keyof PartialDBModel, infer FromDBKey extends keyof Model]
+  ? PartialDBModel extends { [key in ToDBKey]: Exclude<PartialDBModel[key], undefined> }
+    ? { [key in FromDBKey]: Model[key] }
+    : EmptyObject
+  : EmptyObject;
+
+type RemapFromDBKeys<DBModel, Model, PartialDBModel extends Partial<DBModel>, KeyRemap extends [keyof PartialDBModel, keyof Model][]> = Pick<
+  Model,
+  Extract<keyof Model, Exclude<keyof PartialDBModel, KeyRemap[number][0]>>
+> &
+  UnionToIntersection<Exclude<RemapFromDBUnion<DBModel, Model, PartialDBModel, KeyRemap[number]>, EmptyObject>>;
+
+type RemapToDBUnion<DBModel, Model, PartialModel extends Partial<Model>, KeyRemap extends [keyof DBModel, keyof PartialModel]> = KeyRemap extends [
+  infer ToDBKey extends keyof DBModel,
+  infer FromDBKey extends keyof PartialModel
+]
+  ? PartialModel extends { [key in FromDBKey]: Exclude<PartialModel[key], undefined> }
+    ? { [key in ToDBKey]: DBModel[key] }
+    : EmptyObject
+  : EmptyObject;
+
+type RemapToDBKeys<DBModel, Model, PartialModel extends Partial<Model>, KeyRemap extends [keyof DBModel, keyof PartialModel][]> = Pick<
+  DBModel,
+  Extract<keyof DBModel, Exclude<keyof PartialModel, KeyRemap[number][1]>>
+> &
+  UnionToIntersection<Exclude<RemapToDBUnion<DBModel, Model, PartialModel, KeyRemap[number]>, EmptyObject>>;
+
 export interface FromDB<DBModel, Model, FromDBArgs extends any[] = []> {
   (dbModel: DBModel, ...args: FromDBArgs): Model;
 }
 
-export interface SmartFromDB<DBModel, Model, FromDBArgs extends any[] = []> extends FromDB<DBModel, Model, FromDBArgs> {
-  <PartialDBModel extends Partial<DBModel>>(dbModel: PartialDBModel, ...args: FromDBArgs): Pick<Model, Extract<keyof Model, keyof PartialDBModel>>;
+export interface SmartFromDB<DBModel, Model, FromDBArgs extends any[] = [], KeyRemap extends [keyof DBModel, keyof Model][] = []> {
+  (dbModel: DBModel, ...args: FromDBArgs): Model;
+
+  <PartialDBModel extends Partial<DBModel>>(dbModel: PartialDBModel, ...args: FromDBArgs): RemapFromDBKeys<DBModel, Model, PartialDBModel, KeyRemap>;
 }
 
 export interface MapFromDB<DBModel, Model, FromDBArgs extends any[] = []> {
   (dbModels: DBModel[], ...args: FromDBArgs): Model[];
 }
 
-export interface SmartMapFromDB<DBModel, Model, FromDBArgs extends any[] = []> extends MapFromDB<DBModel, Model, FromDBArgs> {
-  <PartialDBModel extends Partial<DBModel>>(dbModels: PartialDBModel[], ...args: FromDBArgs): Pick<
+export interface SmartMapFromDB<DBModel, Model, FromDBArgs extends any[] = [], KeyRemap extends [keyof DBModel, keyof Model][] = []> {
+  (dbModels: DBModel[], ...args: FromDBArgs): Model[];
+
+  <PartialDBModel extends Partial<DBModel>>(dbModels: PartialDBModel[], ...args: FromDBArgs): RemapFromDBKeys<
+    DBModel,
     Model,
-    Extract<keyof Model, keyof PartialDBModel>
+    PartialDBModel,
+    KeyRemap
   >[];
 }
 
@@ -21,16 +63,20 @@ export interface ToDB<DBModel, Model, ToDBArgs extends any[] = []> {
   (model: Model, ...args: ToDBArgs): DBModel;
 }
 
-export interface SmartToDB<DBModel, Model, ToDBArgs extends any[] = []> extends ToDB<DBModel, Model, ToDBArgs> {
-  <PartialModel extends Partial<Model>>(model: PartialModel, ...args: ToDBArgs): Pick<DBModel, Extract<keyof DBModel, keyof PartialModel>>;
+export interface SmartToDB<DBModel, Model, ToDBArgs extends any[] = [], KeyRemap extends [keyof DBModel, keyof Model][] = []> {
+  (model: Model, ...args: ToDBArgs): DBModel;
+
+  <PartialModel extends Partial<Model>>(model: PartialModel, ...args: ToDBArgs): RemapToDBKeys<DBModel, Model, PartialModel, KeyRemap>;
 }
 
 export interface MapToDB<DBModel, Model, ToDBArgs extends any[] = []> {
   (models: Model[], ...args: ToDBArgs): DBModel[];
 }
 
-export interface SmartMapToDB<DBModel, Model, ToDBArgs extends any[] = []> extends MapToDB<DBModel, Model, ToDBArgs> {
-  <PartialModel extends Partial<Model>>(diagrams: PartialModel[], ...args: ToDBArgs): Pick<DBModel, Extract<keyof DBModel, keyof PartialModel>>[];
+export interface SmartMapToDB<DBModel, Model, ToDBArgs extends any[] = [], KeyRemap extends [keyof DBModel, keyof Model][] = []> {
+  (models: Model[], ...args: ToDBArgs): DBModel[];
+
+  <PartialModel extends Partial<Model>>(diagrams: PartialModel[], ...args: ToDBArgs): RemapToDBKeys<DBModel, Model, PartialModel, KeyRemap>[];
 }
 
 export interface SimpleAdapter<DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []> {
@@ -38,9 +84,15 @@ export interface SimpleAdapter<DBModel, Model, FromDBArgs extends any[] = [], To
   fromDB: FromDB<DBModel, Model, FromDBArgs>;
 }
 
-export interface SmartSimpleAdapter<DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []> {
-  toDB: SmartToDB<DBModel, Model, ToDBArgs>;
-  fromDB: SmartFromDB<DBModel, Model, FromDBArgs>;
+export interface SmartSimpleAdapter<
+  DBModel,
+  Model,
+  FromDBArgs extends any[] = [],
+  ToDBArgs extends any[] = [],
+  KeyRemap extends [keyof DBModel, keyof Model][] = []
+> {
+  toDB: SmartToDB<DBModel, Model, ToDBArgs, KeyRemap>;
+  fromDB: SmartFromDB<DBModel, Model, FromDBArgs, KeyRemap>;
 }
 
 export interface MultiAdapter<DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []>
@@ -49,10 +101,15 @@ export interface MultiAdapter<DBModel, Model, FromDBArgs extends any[] = [], ToD
   mapFromDB: MapFromDB<DBModel, Model, FromDBArgs>;
 }
 
-export interface SmartMultiAdapter<DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []>
-  extends SmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs> {
-  mapToDB: SmartMapToDB<DBModel, Model, ToDBArgs>;
-  mapFromDB: SmartMapFromDB<DBModel, Model, FromDBArgs>;
+export interface SmartMultiAdapter<
+  DBModel,
+  Model,
+  FromDBArgs extends any[] = [],
+  ToDBArgs extends any[] = [],
+  KeyRemap extends [keyof DBModel, keyof Model][] = []
+> extends SmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs, KeyRemap> {
+  mapToDB: SmartMapToDB<DBModel, Model, ToDBArgs, KeyRemap>;
+  mapFromDB: SmartMapFromDB<DBModel, Model, FromDBArgs, KeyRemap>;
 }
 
 export interface AdapterOptions {
@@ -121,12 +178,18 @@ export const createSimpleAdapter = <DBModel, Model, FromDBArgs extends any[] = [
   fromDB: fromDBFactory(fromDB, options),
 });
 
-export const createSmartSimpleAdapter = <DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []>(
+export const createSmartSimpleAdapter = <
+  DBModel,
+  Model,
+  FromDBArgs extends any[] = [],
+  ToDBArgs extends any[] = [],
+  KeyRemap extends [keyof DBModel, keyof Model][] = []
+>(
   fromDB: (dbModel: Partial<DBModel>, ...args: FromDBArgs) => Partial<Model>,
   toDB: (model: Partial<Model>, ...args: ToDBArgs) => Partial<DBModel>,
   options: AdapterOptions = {}
   // eslint-disable-next-line sonarjs/no-identical-functions
-): SmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs> => ({
+): SmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs, KeyRemap> => ({
   toDB: toDBFactory(toDB, options),
 
   fromDB: fromDBFactory(fromDB, options),
@@ -144,12 +207,18 @@ export const createMultiAdapter = <DBModel, Model, FromDBArgs extends any[] = []
   mapFromDB: mapFromDBFactory(fromDB, options),
 });
 
-export const createSmartMultiAdapter = <DBModel, Model, FromDBArgs extends any[] = [], ToDBArgs extends any[] = []>(
+export const createSmartMultiAdapter = <
+  DBModel,
+  Model,
+  FromDBArgs extends any[] = [],
+  ToDBArgs extends any[] = [],
+  KeyRemap extends [keyof DBModel, keyof Model][] = []
+>(
   fromDB: (dbModel: Partial<DBModel>, ...args: FromDBArgs) => Partial<Model>,
   toDB: (model: Partial<Model>, ...args: ToDBArgs) => Partial<DBModel>,
   options: AdapterOptions = {}
-): SmartMultiAdapter<DBModel, Model, FromDBArgs, ToDBArgs> => ({
-  ...createSmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs>(fromDB, toDB, options),
+): SmartMultiAdapter<DBModel, Model, FromDBArgs, ToDBArgs, KeyRemap> => ({
+  ...createSmartSimpleAdapter<DBModel, Model, FromDBArgs, ToDBArgs, KeyRemap>(fromDB, toDB, options),
 
   mapToDB: mapToDBFactory(toDB, options),
 
